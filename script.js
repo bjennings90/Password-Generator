4cfc16e513e058896714db02da4319aa263a1e7f
bjennings90 <101131665+bjennings90@users.noreply.github.com>
2022-06-13 11:09:53 -0500

    add while loop, character string, and if statements

--- script.js
+++ script.js
@@ -1,38 +1,58 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = lowerCase.toUpperCase();
-var numbers = ["0","1","2","3","4","5","6","7","8","9"]
-var specialChar = ["#","%","!","@","&","$"]
-console.log(upperCase)
-
-// Write password to the #password input
+//Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
+  
 }
 
-function generatePassword() {
-  //Prompt people to to choose character options
-  var promptPassword = prompt("What number of characters do you want your password to be?");
-  if (promptPassword < 8) {
-    alert("Your password needs to be between 8 and 128 characters.");
-    return;
-  }
-  if (promptPassword > 128) {
-    alert("Your password needs to be between 8 and 128 characters.");
-    return;
-  }
 
+
+// console.log(upperCase)
+function generatePassword() {
+  //Prompt people to choose character options
+  var passLength = ""
+
+  while (isNaN(parseInt(passLength)) || parseInt(passLength) < 8 || parseInt(passLength) > 128) {
+    passLength = prompt("Your password needs to be between 8 and 128 characters.");
+  }
+ 
   var lowerCaseCharacters = confirm("Do you want to have lowercase letters in your password?");
   var upperCaseCharacters = confirm("Do you want to have uppercase letters in your password?");
   var numberCharacters = confirm("Do you want to have numbers in your password?");
-  var specialCharacters = confirm("Do you want to have special charachters in your password?");
+  var specialCharacters = confirm("Do you want to have special characters in your password?");
+
+var characters = ""
+
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = lowerCase.toUpperCase();
+var numbers = "0123456789"
+var specialChar = "!@#$%^&*()_+="
+
+if (lowerCaseCharacters) {
+  characters += lowerCase
+}
+
+if (upperCaseCharacters) {
+  characters += upperCase
+}
+
+if (numberCharacters) {
+  characters += numbers
+}
+
+if (specialCharacters) {
+  characters += specialChar
+}
+console.log(characters)
+
+// Math.floor(Math.random() * (max-min +1) + min);
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
